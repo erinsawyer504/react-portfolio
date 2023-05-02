5f9952716c3058b3f4b2b6267568b9a9d22cd0c8
erinsawyer504 <erinsawyer504@gmail.com>
2023-05-02 17:33:32 -0400

    Stylized the footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,34 +1,32 @@
 import React from 'react';
-import { Message, Segment, Container } from 'semantic-ui-react';
+import { Message, Segment, Container, List } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 function Footer() {
   return (
     <Segment>
       <Container>
-        <div className="page-footer" id="footer">
-          <Message>
-            <div className="ui icon message">
-              <i className="github square icon"></i>
-              <div className="content">
-                <div className="header">
-                  Have you heard about our mailing list?
-                </div>
-                <p>Get the best news in your e-mail every day.</p>
+        <Message>
+          <List divided horizontal size="medium">
+            <List.Item>
+              <div className="ui icon message">
+                <i className="github square icon"></i>
+                <List.Content>
+                  <List.Header>GitHub</List.Header>
+                </List.Content>
               </div>
-            </div>
-            <div className="ui icon message">
-              <i className="linkedin icon"></i>
-              <div className="content">
-                <div className="header">
-                  Have you heard about our mailing list?
-                </div>
-                <p>Get the best news in your e-mail every day.</p>
+            </List.Item>
+            <List.Item>
+              <div className="ui icon message">
+                <i className="linkedin icon"></i>
+                <List.Content>
+                  <List.Header>LinkedIn</List.Header>
+                </List.Content>
               </div>
-            </div>
-          </Message>
+            </List.Item>
+          </List>
           {/* include links to github & linkedin */}
-        </div>
+        </Message>
       </Container>
     </Segment>
   );
