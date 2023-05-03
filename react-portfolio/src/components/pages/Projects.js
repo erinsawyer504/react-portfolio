5174578dd4b257e51c6041011822c641f5c38aea
erinsawyer504 <erinsawyer504@gmail.com>
2023-05-02 21:27:36 -0400

    Added the projects to the project page

--- react-portfolio/src/components/pages/Projects.js
+++ react-portfolio/src/components/pages/Projects.js
@@ -1,30 +1,35 @@
 import React from 'react';
-import {
-    Segment
-  } from 'semantic-ui-react';
-  
+import projects from '../../projects.json'
+import { Segment, Card, Icon } from 'semantic-ui-react';
 
 export default function Project() {
   return (
     <Segment>
-    <div>
-      <h1>Projects Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
-    </div>
-    </Segment> 
+      <Card.Group>
+        {projects.map((project) => (
+          <Card key={project.id}>
+            <div className="image">
+              <img alt={project.name} src={project.image} />
+            </div>
+            <Card.Content>
+              <Card.Header>{project.name}</Card.Header>
+              <Card.Description>{project.description}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <a className="ui basic blue button" href={project.github}>
+                  <Icon name="github" />GitHub
+                </a>
+                {project.deployedapp && (
+                  <a className="ui basic green button" href={project.deployedapp}>
+                    <Icon name="external" />Live Demo
+                  </a>
+                )}
+              </div>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </Segment>
   );
 }
