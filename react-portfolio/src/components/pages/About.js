46a83796fa2ad1bd03f1490a54caf1a6dd96c63d
erinsawyer504 <erinsawyer504@gmail.com>
2023-05-05 20:18:50 -0400

    Added photo of myself and took out home page from portfoliocontainer

--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import {
-  Segment
-} from 'semantic-ui-react';
-
+import { Segment } from 'semantic-ui-react';
 
 export default function About() {
-    return(
-  <Segment>
-      
-      <div id="about-container">       
-      <p className="name">Erin Sawyer</p>
-      <p className="job">Full Stack Web Developer</p>
-      <h2 className="about">About Me</h2>
-      <img src="./assets/blogpic.png"></img>
+  return (
+    <Segment>
+      <div id="about-container">
+        <p className="name">Erin Sawyer</p>
+        <p className="job">Full Stack Web Developer</p>
+        <h2 className="about">About Me</h2>
+        <img src="https://avatars.githubusercontent.com/u/117681165?s=400&u=07ceb9abff9f9fbc0292cec20e0b84e1e6012e65&v=4" alt="Erin Sawyer's profile picture" />
         <p>
-        Intro paragraph about me
-        Additional information about me
-         </p>
-
+          Intro paragraph about me Additional information about me
+        </p>
       </div>
-    </Segment> 
-)}
-  
+    </Segment>
+  );
+}
