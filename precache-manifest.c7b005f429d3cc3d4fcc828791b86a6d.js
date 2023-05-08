a0c7046d3a2ecaf297fc572d7fbb8757418cc157
erinsawyer504 <erinsawyer504@gmail.com>
2023-05-08 14:42:30 -0400

    Updates

---
+++ precache-manifest.c7b005f429d3cc3d4fcc828791b86a6d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3856194c0d6c548dae",
+    "url": "/react-portfolio/static/css/main.73051dc9.chunk.css"
+  },
+  {
+    "revision": "5d3856194c0d6c548dae",
+    "url": "/react-portfolio/static/js/main.5d385619.chunk.js"
+  },
+  {
+    "revision": "b1fa73652834a72765cc",
+    "url": "/react-portfolio/static/js/1.b1fa7365.chunk.js"
+  },
+  {
+    "revision": "d83e0951b70c9524e1cd",
+    "url": "/react-portfolio/static/css/2.b275726f.chunk.css"
+  },
+  {
+    "revision": "d83e0951b70c9524e1cd",
+    "url": "/react-portfolio/static/js/2.d83e0951.chunk.js"
+  },
+  {
+    "revision": "e4de61c68a1de88c89d5",
+    "url": "/react-portfolio/static/js/runtime~main.e4de61c6.js"
+  },
+  {
+    "revision": "26135a70bc96420ecb9b145e9a9cf635",
+    "url": "/react-portfolio/static/media/erin_sawyer_resume1.26135a70.pdf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "3158591a90896500d3a8b1b156c9d068",
+    "url": "/react-portfolio/index.html"
+  }
+];
